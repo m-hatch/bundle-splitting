b7315c6351105edfd5fca039bb4a8e69cd38f379
Montgomery Hatch <montehatch@gmail.com>
2019-05-22 13:30:50 -0400

    Split react and vendor bundles

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,8 @@ const DevPlugins = [
 
 const ProdPlugins = [
   DefinePluginConfig,
-  new CleanWebpackPlugin()
+  new CleanWebpackPlugin(),
+  new webpack.HashedModuleIdsPlugin()
 ];
 
 const CommonPlugins = [
@@ -73,9 +74,37 @@ module.exports = {
     historyApiFallback: true,
   },
   mode: dev ? 'development' : 'production',
-  optimization: !dev ? {
+  optimization: {
     minimize: false,
-  } : {},
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        react: {
+          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+          name: 'react',
+          chunks: 'all'
+        },
+        vendor: {
+          test(mod) {
+            // exclude anything outside node modules
+            if (!mod.context.includes('node_modules')) {
+              return false;
+            }
+
+            // exclude react and react-dom
+            if (/[\\/]node_modules[\\/](react|react-dom)[\\/]/.test(mod.context)) {
+              return false;
+            }
+
+            // return all other node modules
+            return true;
+          },
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  },
   plugins: dev
     ? DevPlugins.concat(CommonPlugins)
     : ProdPlugins.concat(CommonPlugins)
